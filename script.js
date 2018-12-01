8289e652b3930421d6311096e325dfb9c5ed1a7f
Elizabeth Prins <elizabeth@driebit.nl>
2018-12-01 13:56:37 +0100

    add bubble wrap

--- script.js
+++ script.js
@@ -1,65 +1,76 @@
-$(document).ready(function() {
+(function() {
+    document.addEventListener("DOMContentLoaded", function() {
 
-  $("div").on("mouseenter", function() {
-    if (this.id.includes("white") == false) {
-      $(this).fadeOut('slow');
-    };
-  });
+        // ELEMENTS
+        const toothList = document.getElementsByClassName('tooth');
+        const eyebrownList = document.getElementsByClassName('eyebrown');
+        const sandList = document.getElementsByClassName('sand');
+        const lightergreyList = document.getElementsByClassName('lightergrey');
+        const lightblackList = document.getElementsByClassName('lightblack');
+        const jawList = document.getElementsByClassName('jaw');
+        const bubbles = document.getElementsByTagName('span');
 
-  $(".tooth").on("mouseover", function() {
-    document.getElementById('america').play();
-  });
+        // SOUNDS
+        const america = document.getElementById('america');
+        const thanks = document.getElementById('thanks');
+        const ahhh = document.getElementById('ahhh');
+        const delish = document.getElementById('delish');
+        const courage = document.getElementById('courage');
+        const bubble = document.getElementById('bubble');
 
-  $(".eyebrown").on("mouseover", function() {
-    document.getElementById('thanks').play();
-  });
+        // RESET STUFF
+        const resetBtn = document.getElementById('reset-btn');
+        const resetSound = document.getElementById('reset');
+        const resetBg = document.getElementById('background');
+        const resetSlider = document.getElementById('slider');
 
-  $(".sand").on("mouseover", function() {
-    document.getElementById('ahhh').play();
-  });
+        resetBtn.addEventListener('click', function() {
+              resetSound.play();
 
-  $(".lightergrey").on("mouseover", function() {
-    document.getElementById('thanks').play();
-  });
+              resetBg.classList.add('is-visible');
+              resetSlider.classList.add('is-visible');
 
-  $(".lightblack").on("mouseover", function() {
-    document.getElementById('delish').play();
-  });
+              setTimeout(() => {
+                  window.location.reload();
+              }, 5000)
+        })
 
-  $(".jaw").on("mouseover", function() {
-    document.getElementById('courage').play();
-  });
+        const popHandler = function() {
+            if (this.classList.contains('popped')) return;
 
-  $(".button1")
-  .on("mouseover", function() {
-    $(this).animate({
-      width: "150px",
-      height: "50px",
-      fontSize: "18px",
-    }, 1500);
-  })
-  .on("mouseleave", function() {
-    $(this).animate({
-      "bottom": "+=40px",
-    }, "slow");
-  })
-  .click(function() {
-    $(".button2").show();
-    $(".button1").hide();
-  });
+            bubble.currentTime = 0
+            bubble.play();
 
-  $(".button2").click(function() {
-    $(".button3").show();
-    $(".button2").hide();
-  });
+            setTimeout(() => {
+                this.classList.add('popped');
+            }, 0);
+        }
 
-  $(".button3").click(function() {
-    document.getElementById('reset').play();
-    $("img").show();
-    $.when($("#slider").animate({
-      "bottom": "-=100%",
-    }, 5000)).done(function() {
-      location.reload();
+        const listener = window.innerWidth < 600 ? 'touchstart' : 'mouseenter';
+
+        function connectToEach(collection, handler) {
+            const elements = Array.from(collection);
+            return elements.map(el => el.addEventListener(listener, handler));
+        }
+
+        function popBubbles(collection) {
+            connectToEach(collection, popHandler);
+        }
+
+        function connectSoundToElements(collection, sound) {
+            connectToEach(collection, function() {
+                if (this.classList.contains('popped')) return;
+                sound.play();
+            })
+        }
+
+        popBubbles(bubbles);
+
+        connectSoundToElements(toothList, america);
+        connectSoundToElements(eyebrownList, thanks);
+        connectSoundToElements(sandList, ahhh);
+        connectSoundToElements(lightergreyList, thanks);
+        connectSoundToElements(lightblackList, delish);
+        connectSoundToElements(jawList, courage);
     });
-  });
-});
+})();

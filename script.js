35bd21703677d16edb8c14238b0bb1088e3d6d4f
Elizabeth Prins <elizabeth@driebit.nl>
2018-12-02 22:25:15 +0100

    handle touch events

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
     document.addEventListener("DOMContentLoaded", function() {
 
         // ELEMENTS
+        const president = document.getElementById('president');
         const toothList = document.getElementsByClassName('tooth');
         const eyebrownList = document.getElementsByClassName('eyebrown');
         const sandList = document.getElementsByClassName('sand');
@@ -25,46 +26,98 @@
         const resetSlider = document.getElementById('slider');
 
         resetBtn.addEventListener('click', function() {
-              resetSound.play();
+              tryToPlay(resetSound);
 
               resetBg.classList.add('is-visible');
               resetSlider.classList.add('is-visible');
 
               setTimeout(() => {
                   window.location.reload();
-              }, 5000)
+              }, 5000);
         })
 
-        const popHandler = function() {
+        const isMobile = window.innerWidth < 600;
+
+        const handleMouse = function() {
             if (this.classList.contains('popped')) return;
 
-            bubble.currentTime = 0
-            bubble.play();
+            bubble.currentTime = 0;
+            tryToPlay(bubble);
 
             setTimeout(() => {
                 this.classList.add('popped');
             }, 0);
         }
 
-        const listener = window.innerWidth < 600 ? 'touchstart' : 'mouseenter';
+        const handleTouch = function() {
+            if (this.classList.contains('popped')) return;
+
+            bubble.currentTime = 0;
+            tryToPlay(bubble);
+        }
+
+        const handleDrag = function() {
+            event.preventDefault();
+
+            const activeElement = Array.from(bubbles).find(el => {
+                let c = el.getBoundingClientRect();
+
+                if ((c.x <= event.pageX && c.x + c.width >= event.pageX) &&
+                (c.y <= event.pageY && c.y + c.height >= event.pageY)) {
+                    return el;
+                }
+            })
+
+            if (!activeElement || activeElement.classList.contains('popped')) return;
+
+            bubble.currentTime = 0;
+            tryToPlay(bubble);
+
+            setTimeout(() => {
+                activeElement.classList.add('popped');
+            }, 0);
+        }
+
+        function tryToPlay(sound) {
+            let promise = sound.play();
+
+            if (promise !== undefined) {
+                promise.then(_ => {
+                    // sound plays
+                }).catch(error => {
+                    // autoplay was prevented
+                });
+            }
+        }
 
         function connectToEach(collection, handler) {
             const elements = Array.from(collection);
+            const listener = isMobile ? 'touchstart' : 'mouseenter';
             return elements.map(el => el.addEventListener(listener, handler));
         }
 
-        function popBubbles(collection) {
-            connectToEach(collection, popHandler);
+        function mouseBubbles() {
+            !isMobile && connectToEach(bubbles, handleMouse);
+        }
+
+        function touchBubbles() {
+            isMobile && connectToEach(bubbles, handleTouch);
+        }
+
+        function dragBubbles() {
+            isMobile && president.addEventListener('touchmove', handleDrag);
         }
 
         function connectSoundToElements(collection, sound) {
             connectToEach(collection, function() {
                 if (this.classList.contains('popped')) return;
-                sound.play();
+                tryToPlay(sound);
             })
         }
 
-        popBubbles(bubbles);
+        mouseBubbles();
+        touchBubbles();
+        dragBubbles();
 
         connectSoundToElements(toothList, america);
         connectSoundToElements(eyebrownList, thanks);
